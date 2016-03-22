30433f9f99c07f748b0eaa954eddcd29b73eaa0c
fetorres <fetorres@groupon.com>
2016-03-22 19:03:31 -0300

    update

--- components/Redux/modules/index.js
+++ components/Redux/modules/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import account from './account';
+import sidebar from './sidebar';
 // import notifications from './notifications';
 
 export default combineReducers({
   account,
+  sidebar,
 });
