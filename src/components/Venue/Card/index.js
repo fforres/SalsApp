5a0ed1cfe2c144b3a4deccddfccd247656c56461
fetorres <fetorres@groupon.com>
2016-03-23 19:35:42 -0300

    adding touchablehighlight

--- src/components/Venue/Card/index.js
+++ src/components/Venue/Card/index.js
@@ -1,4 +1,6 @@
-import React, { Component, StyleSheet, PropTypes, View, Text, Image} from 'react-native';
+import React, { Component, StyleSheet, PropTypes, View, Text, Image, TouchableHighlight } from 'react-native';
+import Dimensions from 'Dimensions';
+const window = Dimensions.get('window');
 
 // TODO: Add parallax
 class venueCard extends Component {
@@ -9,38 +11,70 @@ class venueCard extends Component {
   };
   componentWillMount() {
   }
+  onPressCard(){
+    const _this = this;
+    console.log(_this);
+  }
   render(){
     return (
-      <View style={styles.card}>
-        <Image
-            source={{uri: this.props.profileImage}}
-            style={styles.backgroundImage}
-        >
-          <Text style={styles.name}>{this.props.name}</Text>
-          <Text style={styles.address}>{this.props.address}</Text>
-        </Image>
-      </View>
+        <View style={styles.card}>
+          <View style={styles.fullSize}>
+            <Image
+                source={{uri: this.props.profileImage}}
+                style={styles.backgroundImage}
+            />
+          </View>
+          <View style={[styles.welcome, styles.fullSize]}>
+            <View>
+              <Text style={styles.name}>{this.props.name}</Text>
+              <Text style={styles.address}>{this.props.address}</Text>
+            </View>
+            <View style={[styles.buttonRight]}>
+              <TouchableHighlight
+                  onPress={this.onPressCard}
+                  style={[styles.flatButtonWrapper]}
+                  underlayColor={'rgba(150,150,150,0.4)'}
+              >
+                <View style={[styles.flatButton]}>
+                  <Text style={[styles.flatButtonText]}>
+                    VER
+                  </Text>
+                </View>
+              </TouchableHighlight>
+
+            </View>
+          </View>
+        </View>
     );
   }
 }
 
-
 let styles = StyleSheet.create({
   card: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    height:170
+    height:window.height/3.2,
+  },
+  fullSize: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
   backgroundImage: {
     flex: 1,
-    height: 170,
-    resizeMode: 'cover', // or 'stretch'
-    justifyContent: 'center',
-    alignItems: 'center',
+    resizeMode: 'cover',
+    opacity: 100,
+  },
+  welcome: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+    backgroundColor: 'rgba(0,0,0,0.3)',
   },
   name:{
     backgroundColor: 'transparent',
+    // fontFamily: 'Bebas Neue',
     color: 'white',
     fontSize: 25,
   },
@@ -49,8 +83,24 @@ let styles = StyleSheet.create({
     color: 'white',
     fontSize: 15,
   },
-})
-
-
+  flatButtonWrapper: {
+    width: window.width/6,
+  },
+  flatButton: {
+    borderColor: 'rgba(141,164,175,1)',
+    borderWidth: 2,
+    padding: 4,
+  },
+  flatButtonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  buttonRight: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  }
+});
 
 export default venueCard;
